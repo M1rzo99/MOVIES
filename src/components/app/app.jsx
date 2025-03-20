efb15e71a2968f79d012df1849351c5d64972290
lucas <accaunoff99@gmail.com>
2025-03-21 00:13:51 +0900

    FEAT: used lazy our project

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,28 +1,27 @@
+import { lazy, Suspense } from "react";
 import React from "react";
 import Navbar from "../navber/navbar";
 import { Route, Routes } from "react-router-dom";
-import HomePage from "../../pages/home-page";
-import DeteliedPage from "../../pages/detelied-page";
-import NotFoundPage from "../../pages/not-found-page";
 
-import PopularPage from "../../pages/popular-page";
-import TrandingPage from "../../pages/tranding-page";
+const NotFoundPage = lazy(() => import("../../pages/home-page"));
+const HomePage = lazy(() => import("../../pages/home-page"));
+const TrandingPage = lazy(() => import("../../pages/tranding-page"));
+const PopularPage = lazy(() => import("../../pages/popular-page"));
+const DeteliedPage = lazy(() => import("../../pages/detelied-page"));
+
 const App = () => {
-  // const movieServie = new MovieService()
-  // movieServie.getAllPopular().then(data=>console.log(data));
-  // movieServie.getAllTranding().then(data=>console.log(data))
-  // movieServie.getAllDetelies(12).then(data=>console.log(data))
-
   return (
     <div className="app">
       <Navbar />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/tranding" element={<TrandingPage />} />
-        <Route path="/movie/:movieId" element={<DeteliedPage />} />
-        <Route path="*" element={<NotFoundPage />} />
-        <Route path="/popular" element={<PopularPage />} />
-      </Routes>
+      <Suspense>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/tranding" element={<TrandingPage />} />
+          <Route path="/movie/:movieId" element={<DeteliedPage />} />
+          <Route path="*" element={<NotFoundPage />} />
+          <Route path="/popular" element={<PopularPage />} />
+        </Routes>
+      </Suspense>
     </div> // /movie/: dinamik id chiqarish un bajaramiz
   );
 };
